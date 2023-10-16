06c2eaa5cff48f9e7bb8a8831c724cfee1280840
HWoool <gksdnf24@naver.com>
2023-10-16 21:02:26 +0900

    KHJ file

---
+++ src/main/java/com/org/makgol/boards/vo/BoardVo.java
@@ -0,0 +1,67 @@
+package com.org.makgol.boards.vo;
+
+import java.time.LocalDate;
+
+public class BoardVo {
+
+	int id;
+	String user_id;
+	int hit;
+	String title;
+	LocalDate date;
+	String contents;
+	String category;
+	int sympathy;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getSympathy() {
+		return sympathy;
+	}
+	public void setSympathy(int sympathy) {
+		this.sympathy = sympathy;
+	}
+	
+	
+	
+}
