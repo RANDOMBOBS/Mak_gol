d96026b683f11ab64571efece6cd92cec5a10ead
HWoool <gksdnf24@naver.com>
2023-10-25 20:04:34 +0900

    down

--- src/main/java/com/org/makgol/admin/Service/AdminService.java
+++ src/main/java/com/org/makgol/admin/Service/AdminService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.org.makgol.admin.dao.AdminDao;
+import com.org.makgol.admin.Dao.AdminDao;
 import com.org.makgol.users.vo.UserVo;
 
 @Service
@@ -14,12 +14,10 @@ public class AdminService {
 	@Autowired
 	AdminDao adminDao;
 	
-	/** User리스트 전체 SELECT **/
 	public List<UserVo> getUserList() {
 		return adminDao.selectAllUserList();
 	}
 	
-	/** user 등급 수정 UPDATE **/
 	public int modGrade(UserVo userVo) {
 		return adminDao.UpdateGrade(userVo);
 	}
