85a8b8b34c101a940b38651243636dcaa854db80
HyoojinK <kyg0328@naver.com>
2023-10-30 15:24:12 +0900

    1030 hyo

--- src/main/java/com/org/makgol/boards/vo/BoardVo.java
+++ src/main/java/com/org/makgol/boards/vo/BoardVo.java
@@ -2,6 +2,9 @@ package com.org.makgol.boards.vo;
 
 import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
 public class BoardVo {
 
 
@@ -17,66 +20,6 @@ public class BoardVo {
 	String photo;
 	
 	
-	public int getB_id() {
-		return b_id;
-	}
-	public void setB_id(int b_id) {
-		this.b_id = b_id;
-	}
-	public int getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public LocalDate getDate() {
-		return date;
-	}
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-	public String getContents() {
-		return contents;
-	}
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public int getSympathy() {
-		return sympathy;
-	}
-	public void setSympathy(int sympathy) {
-		this.sympathy = sympathy;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPhoto() {
-		return photo;
-	}
-	public void setPhoto(String photo) {
-		this.photo = photo;
-	}
 
 	
 }
